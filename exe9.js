4639beb8f18df6793bbccabf1ae425883e1bcdd7
Fabio Henrique <binhopvp@hotmail.com>
2020-08-25 19:39:11 -0300

    Exe 9 versão final

--- exe9.js
+++ exe9.js
@@ -8,5 +8,18 @@ function chefe() {
 }
 
 function exe9(num) {
-    
+    var soma = 0
+    for(var divisor=num; divisor >= 1; divisor--) { //divisor começa no 6 e vai até o 1
+        if(num % divisor == 0) {
+            soma = soma + divisor
+        }
+    }
+
+   // for(var divisor = 1; divisor <= num; divisor++) { //divisor começa no 1 e vai até o 6
+   //     if(num % divisor == 0) {
+   //         soma = soma + divisor
+   //     }
+   // }
+   
+    console.log(`A soma dos divisores de ${num} é ${soma}`)
 }
